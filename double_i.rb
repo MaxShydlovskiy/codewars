4ec246dabfa122c62e3e7c49195a4d632f7c7017
Max <76831095+MaxShydlovskiy@users.noreply.github.com>
2021-03-09 23:45:48 +0200

    Add files via upload

---
+++ double_i.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/ruby
+=begin
+def double_integer(i)
+  
+end
+=end
+def double_integer(i)
+  i.to_i * 2
+end
---
+++ kata_greeting_boss.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby
+=begin
+def greet(name,owner)
+  
+end
+=end
+
+def greet(name,owner)
+  name == owner ? 'Hello boss' : 'Hello guest'
+end
